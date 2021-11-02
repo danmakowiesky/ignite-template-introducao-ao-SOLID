3c04bf449f8eaf11c85f1da25ae35c13f15a8f06
Daniel Makowiesky <daniel.makowiesky@smarketsolutions.com.br>
2021-11-02 10:24:32 -0300

    swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
+import swaggerSetup from "../swaggerSetup.json";
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerSetup));
 
 app.use("/users", usersRoutes);
 
